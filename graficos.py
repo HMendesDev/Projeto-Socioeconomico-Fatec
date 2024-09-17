903bacb34e378e8e20d8351526bbb01d7dd4488e
Hugo Mendes <hugogs.mendes@gmail.com>
2024-09-17 14:21:04 -0300

    Em desenvolvimento

--- graficos.py
+++ graficos.py
@@ -1,15 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+valores = []
+colunas_para_remover = ["ID", "Hora de início", "Hora de conclusão", "Nome", "Email", "Hora da última modificação"]
 tabela = pd.read_excel("DadosPSE.xlsx")
-tabela = tabela.drop("ID",axis=1)
-tabela = tabela.drop("Hora de início",axis=1)
-tabela = tabela.drop("Hora de conclusão",axis=1)
-tabela = tabela.drop("Nome",axis=1)
-tabela = tabela.drop("Email",axis=1)
-tabela = tabela.drop("Hora da última modificação",axis=1)
-
-
-for i in range (3):
-    plt.pie(tabela["Pai"].value_counts(),labels=tabela["Pai"].value_counts().index,autopct="%1.1f%%")
+tabela = tabela.drop(columns=colunas_para_remover)
+tabela.columns = tabela.columns.str.strip()
+print(len(tabela.columns))
+for j in range(len(tabela.columns)):
+    valores.append(tabela.columns[j])
+for i in range (1):
+    plt.pie(tabela[valores[i]].value_counts(),labels=tabela[valores[i]].value_counts().index,autopct="%2.1f%%")
+    plt.title("Quais fontes de entretenimento cultural você usa?")
     plt.savefig(f'Graficos/grafico{i}.pdf',format = 'pdf')
