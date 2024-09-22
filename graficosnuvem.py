1bfacb349cfadf9d19782979ce357bfc6e8c52e3
Hugo Mendes <hugogs.mendes@gmail.com>
2024-09-22 12:12:31 -0300

    Falta uma nuvem de palavras pt 2

--- graficosnuvem.py
+++ graficosnuvem.py
@@ -2,18 +2,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+#Nuvem de palavras das Empresas
+# nuvem = pd.read_csv("NuvemEmpresas.txt")
+# print(nuvem)
+# text = " ".join(i for i in nuvem["Empresas:"])
+# print(text)
+# eliminar = set(STOPWORDS)
+# eliminar = eliminar.update(["Não", "se", "sea", "aplica","eu","meus","você", "meu",
+# "e","em","as","do","na","e","era","fui","é","no","de","Tenho","Estou","como","para"])
+# wordcloud = WordCloud(stopwords=eliminar,background_color= "black",width=1600,
+# height=800).generate(text)
+# plt.figure()
+# plt.imshow(wordcloud, interpolation="bilinear")
+# plt.axis("off")
+# plt.savefig('Graficos/graficonuvem1.jpeg',format = 'jpeg')
 
-nuvem = pd.read_csv("NuvemEmpresas.txt")
-print(nuvem)
-text = " ".join(i for i in nuvem["Empresas:"])
-print(text)
-eliminar = set(STOPWORDS)
-eliminar = eliminar.update(["Não", "se", "sea", "aplica"])
-wordcloud = WordCloud(stopwords=eliminar,background_color= "black",width=1600,
-height=800).generate(text)
-plt.figure()
-plt.imshow(wordcloud, interpolation="bilinear")
-plt.axis("off")
-plt.savefig('Graficos/graficonuvem1.jpeg',format = 'jpeg')
-
-
+#Nuvem de palavras dos Sonhos
+nuvem2 = pd.read_csv("NuvemSonhos.txt")
+print(nuvem2)
+text2 = " ".join(i for i in nuvem2["Sonhos:"])
+print(text2)
